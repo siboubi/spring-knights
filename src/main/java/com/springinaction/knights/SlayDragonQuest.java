0a557b9708f41461d0bda00100ee2573fa0eb02a
Steve Iboubi <steve.iboubibangole@us.sogeti.com>
2013-01-22 17:17:09 -0600

    Add output line to SlayDragonQuest

--- src/main/java/com/springinaction/knights/SlayDragonQuest.java
+++ src/main/java/com/springinaction/knights/SlayDragonQuest.java
@@ -2,6 +2,6 @@ package com.springinaction.knights;
 
 class SlayDragonQuest implements Quest {
 	public void embark() throws QuestException {
-		
+		System.out.println("Knight slayed the dragon");
 	}
 }
